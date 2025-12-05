6ecc75fa426b1cd3e87e7e953a879ebbabca5bd4
lanaasep19-tech <lanaasep19@gmail.com>
2025-12-05 10:32:18 +0700

    Configure temporary paths for Vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,11 @@
 <?php
 
+// Vercel root directory
+define('VERCEL_ROOT_PATH', __DIR__);
+
+// Set a writable path for Laravel storage
+// Vercel only allows writing to the /tmp directory
+define('LARAVEL_STORAGE_PATH', '/tmp/storage');
+
+// Bootstrap Laravel
 require __DIR__ . '/../public/index.php';
