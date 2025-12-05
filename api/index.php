25758752661b4ab3f97161fecc7fe9b4a0dc0305
lanaasep19-tech <lanaasep19@gmail.com>
2025-12-05 10:47:32 +0700

    Fix Vercel writable paths

--- api/index.php
+++ api/index.php
@@ -1,11 +1,8 @@
 <?php
 
-// Vercel root directory
-define('VERCEL_ROOT_PATH', __DIR__);
-
-// Set a writable path for Laravel storage
-// Vercel only allows writing to the /tmp directory
-define('LARAVEL_STORAGE_PATH', '/tmp/storage');
+// Define a writable path for Laravel's storage.
+// Vercel only allows writing to the /tmp directory.
+define('LARAVEL_STORAGE_PATH', '/tmp');
 
 // Bootstrap Laravel
 require __DIR__ . '/../public/index.php';
