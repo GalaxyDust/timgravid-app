07b6374b8d725699c750c19a3679a71245325cdf
lanaasep19-tech <lanaasep19@gmail.com>
2025-12-05 10:54:51 +0700

    FIX: Include Composer autoload

--- api/index.php
+++ api/index.php
@@ -1,9 +1,14 @@
 <?php
 
-// Muat bootstrap Vercel
+// [INI PERBAIKANNYA]
+// Muat Autoloader Composer terlebih dahulu
+require __DIR__ . '/../vendor/autoload.php';
+
+
+// Sekarang baru muat bootstrap Vercel
 $app = require __DIR__.'/../bootstrap/vercel.php';
 
-// Handle request
+// Handle request (kode ini tetap sama)
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
